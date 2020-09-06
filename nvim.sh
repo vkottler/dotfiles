6bafcecc39a8963b3d9556ff81934239fbb9081e
vkottler <vaughnkottler@gmail.com>
2020-09-06 02:02:01 -0700

    better usage of app image

--- nvim.sh
+++ nvim.sh
@@ -2,16 +2,22 @@
 
 APP_IMAGE=nvim.appimage
 HOME_BIN=$HOME/bin
+mkdir -p $HOME_BIN
 
 # download the app image
-mkdir -p ~/bin
 wget https://github.com/neovim/neovim/releases/download/nightly/$APP_IMAGE -O $HOME_BIN/$APP_IMAGE
 
 # make executable and add symbolic links 
-chmod +x $HOME_BIN/$APP_IMAGE
-ln -s $HOME_BIN/$APP_IMAGE $HOME_BIN/neovim
-ln -s $HOME_BIN/neovim $HOME_BIN/nvim
+chmod u+x $HOME_BIN/$APP_IMAGE
+$HOME_BIN/$APP_IMAGE --appimage-extract
+rm -rf $HOME_BIN/_nvim
+mv $(pwd)/squashfs-root $HOME_BIN/
+mv $HOME_BIN/squashfs-root $HOME_BIN/_nvim
+rm $HOME_BIN/$APP_IMAGE
+
+ln -fs $HOME_BIN/_nvim/AppRun $HOME_BIN/neovim
+ln -fs $HOME_BIN/neovim $HOME_BIN/nvim
 
 # link standard vimrc
 mkdir -p ~/.config/nvim
-ln -s $(pwd)/init.vim ~/.config/nvim/
+ln -fs $(pwd)/init.vim ~/.config/nvim/
