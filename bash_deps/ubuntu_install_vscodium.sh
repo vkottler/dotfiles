1b33eac9e92fb85440a7f7de035ddcfcb55cf6d7
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2020-09-30 11:11:41 -0700

    don't install extensions by default for now

--- bash_deps/ubuntu_install_vscodium.sh
+++ bash_deps/ubuntu_install_vscodium.sh
@@ -2,5 +2,5 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs/ vscodium main' | sudo tee --append /etc/apt/sources.list.d/vscodium.list
 sudo apt update && sudo apt install codium
 
-code --install-extension vscodevim.vim
-code --enable-proposed-api vscodevim.vim
+#codium --install-extension vscodevim.vim
+#codium --enable-proposed-api vscodevim.vim
