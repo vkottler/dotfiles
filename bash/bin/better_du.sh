26ac05ecd1a45d223e37e2da60c31660909b8b00
vkottler <vaughnkottler@gmail.com>
2020-10-25 14:56:30 -0700

    improve vdu

--- bash/bin/better_du.sh
+++ bash/bin/better_du.sh
@@ -15,10 +15,39 @@ for f in *; do
 done
 MAX_LEN=$((MAX_LEN+4))
 
-for f in *; do
+FILES=`ls -A`
+declare -a RESULTS
+for f in $FILES; do
 	CURR_LEN=${#f}
 	NUM_SPACES=$((MAX_LEN-CURR_LEN))
 	printf $f
 	print_spaces $NUM_SPACES
-	du -chL $f 2>/dev/null | tail -n 1
+	RESULT=`du -chL $f 2>/dev/null | tail -n 1 | sed -e 's/\s.*$//'`
+	RESULTS+=($RESULT)
+	echo $RESULT
 done
+
+echo "----------------------------------------"
+
+# build total
+TOTAL_SIZE=0.0
+for ELEM in "${RESULTS[@]}"; do
+	NUM=${ELEM::-1}
+	ITER_SIZE=0.0
+	case "$ELEM" in
+		*K)
+			ITER_SIZE=$(echo "$NUM * 1024" | bc)
+		;;
+		*M)
+			ITER_SIZE=$(echo "$NUM * 1024 * 1024" | bc)
+		;;
+		*G)
+			ITER_SIZE=$(echo "$NUM * 1024 * 1024 * 1024" | bc)
+		;;
+	esac
+	TOTAL_SIZE=$(echo "$TOTAL_SIZE + $ITER_SIZE" | bc)
+done
+
+# print total
+RESULT=`numfmt --to=iec-i --suffix=B $TOTAL_SIZE`
+echo "total: $RESULT"
