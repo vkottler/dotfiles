0677efe0fc63bf3160562375390b72cd087f0f89
Vaughn Kottler <vaughnkottler@gmail.com>
2020-12-18 21:35:25 -0600

    don't auto-install wsl.bat for wsl

--- bash/links/link_wsl.sh
+++ bash/links/link_wsl.sh
@@ -2,4 +2,4 @@ link_dep wsl.sh
 
 # trying to start stuff automatically is incredibly jank in wsl
 sudo cp -f $(pwd)/00-wsl.conf /etc/sudoers.d/00-wsl
-cp -f $(pwd)/wsl.bat $APP_DATA/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup/wsl.bat
+# cp -f $(pwd)/wsl.bat $APP_DATA/Roaming/Microsoft/Windows/Start\ Menu/Programs/Startup/wsl.bat
