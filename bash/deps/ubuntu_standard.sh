7908908cf4650a162875f13b3f43d165d7d9b893
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2021-02-23 15:35:58 -0800

    add a python version export for ubuntu 16.04

--- bash/deps/ubuntu_standard.sh
+++ bash/deps/ubuntu_standard.sh
@@ -88,3 +88,10 @@ if ! shopt -oq posix; then
 fi
 
 export XDG_CONFIG_HOME=$HOME/.config
+
+# set some things if we're on a deprecated platform
+if [[ `uname -v` == *"Ubuntu"* ]]; then
+	if lsb_release -r | grep 16.04 >/dev/null 2>&1 ; then
+		export PYTHON_VERSION=3.7
+	fi
+fi
