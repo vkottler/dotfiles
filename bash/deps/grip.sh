239c29b589f56a3fae9dddd32df4a5d458202b92
Vaughn Kottler <vaughnkottler@gmail.com>
2021-10-10 23:10:52 -0700

    Make 'md' host on all interfaces

--- bash/deps/grip.sh
+++ bash/deps/grip.sh
@@ -1,1 +1,1 @@
-alias md='pkill --signal 9 grip || true && grip --pass `cat $HOME/grip_access_token.txt` --quiet -b &'
+alias md='pkill --signal 9 grip || true && grip --pass `cat $HOME/grip_access_token.txt` --quiet -b . 0.0.0.0:0 &'
