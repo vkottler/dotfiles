90fe59d0a405eae520e8155cd97361a9f3011d0c
Vaughn Kottler <Vaughn.Kottler@spacex.com>
2022-04-03 19:15:58 -0700

    Fix link_ubuntu

--- bash/links/link_ubuntu.sh
+++ bash/links/link_ubuntu.sh
@@ -1,4 +1,5 @@
-# need to re-evaluate if this is something worth doing
+# We don't care if this fails.
+set +e
 
 if command -v gsettings >/dev/null; then
 	gsettings set org.freedesktop.Tracker.Miner.Files crawling-interval -2  # Default: -1
@@ -8,3 +9,5 @@ fi
 if command -v tracker >/dev/null; then
 	tracker reset --hard
 fi
+
+set -e
