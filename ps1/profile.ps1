e373e097290e23e0f56236ba3da9391ae752e274
Vaughn Kottler <vaughnkottler@gmail.com>
2022-05-08 14:37:23 -0700

    Add chocolatey to powershell profile

--- ps1/profile.ps1
+++ ps1/profile.ps1
@@ -51,3 +51,13 @@ Set-Alias -Name code -Value code-insiders.cmd
 Set-Alias -Name vscode -Value code-insiders.cmd
 
 Write-Output $PSVersionTable.PSVersion
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
