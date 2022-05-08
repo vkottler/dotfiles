a38d9a24fa04810b8daab9413090dd8b0ff16f74
Vaughn Kottler <vaughnkottler@gmail.com>
2022-05-08 14:25:16 -0700

    Update Windows installs

--- ps1/choco_installs.ps1
+++ ps1/choco_installs.ps1
@@ -2,37 +2,19 @@
 #requires -runasadministrator
 
 # install things that we will probably always use on a windows box
-choco upgrade -y microsoft-windows-terminal git rust vlc slack chromium carnac
-choco upgrade -y screentogif drawio discord spotify audacity bitwig
+choco upgrade -y microsoft-windows-terminal git vlc
+choco upgrade -y discord spotify
 choco upgrade -y neovim
-choco upgrade -y obs-studio
-choco upgrade -y python
 choco upgrade -y winrar
-choco upgrade -y make
 
-# VSCodium isn't usable, see:
-# https://github.com/VSCodium/vscodium/discussions/832#discussioncomment-2225387
-# choco upgrade -y vscodium --pre
-# install base extensions
-# codium --install-extension asvetliakov.vscode-neovim
-# codium --install-extension dunstontc.viml
-
-# install Visual Studio Code
-choco upgrade -y vscode-insiders
-code-insiders.cmd --install-extension asvetliakov.vscode-neovim
-code-insiders.cmd --install-extension dunstontc.viml
-code-insiders.cmd --install-extension ms-vscode-remote.vscode-remote-extensionpack
-code-insiders.cmd --install-extension ms-python.python
+# dog house
+#choco upgrade -y slack chromium
 
 # install neovim plugin manager
 $PlugDest = "$HOME\vimfiles\autoload\plug.vim"
 if ( -Not ( Test-Path $PlugDest ) ) {
 	iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim | ni $PlugDest -Force
 }
-
-# python global packages
-pip install --upgrade pip
-pip install --upgrade grip
 
 # set git identity
 git config --global user.email "vaughnkottler@gmail.com"
---
+++ ps1/choco_installs_dev.ps1
@@ -0,0 +1,33 @@
+#requires -version 4.0
+#requires -runasadministrator
+
+# languages and tools
+choco upgrade -y rust python make
+
+# VSCodium isn't usable, see:
+# https://github.com/VSCodium/vscodium/discussions/832#discussioncomment-2225387
+# choco upgrade -y vscodium --pre
+# install base extensions
+# codium --install-extension asvetliakov.vscode-neovim
+# codium --install-extension dunstontc.viml
+
+# install Visual Studio Code
+choco upgrade -y vscode-insiders
+code-insiders.cmd --install-extension asvetliakov.vscode-neovim
+code-insiders.cmd --install-extension dunstontc.viml
+code-insiders.cmd --install-extension ms-vscode-remote.vscode-remote-extensionpack
+code-insiders.cmd --install-extension ms-python.python
+
+# python global packages
+pip install --upgrade pip
+pip install --upgrade grip
+
+choco upgrade -y screentogif drawio
+
+# music dev
+choco upgrade -y audacity bitwig
+
+# video dev
+choco upgrade -y obs-studio screentogif carnac
+
+choco list -localonly
