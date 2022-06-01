8115d5d4a53a13b2798707e9f4f9158191964e41
Vaughn Kottler <vaughnkottler@gmail.com>
2022-06-01 01:13:17 -0700

    Add setup openssl script

---
+++ bash/setup_openssl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPO=`git rev-parse --show-toplevel`
+source $REPO/bash/common.sh
+
+PROJ=openssl
+
+clone_third_party_github $PROJ $PROJ
+
+pushd $THIRD_PARTY/$PROJ >/dev/null
+
+./Configure $PREFIX_ARG --openssldir=$HOME/.config/ssl
+make -j
+make test
+make install
+
+popd >/dev/null
