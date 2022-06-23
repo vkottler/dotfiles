7aa5729f666650ea668eb59a59d00233603c6e95
Vaughn Kottler <vaughnkottler@gmail.com>
2022-06-23 07:53:35 +0000

    More work on Samba scripts

--- bash/mounting/create_raid.sh
+++ bash/mounting/create_raid.sh
@@ -8,8 +8,8 @@ source $CWD/common.sh
 LEVEL=mirror
 FS=ext4
 
-DRIVE_1=sdd
-DRIVE_2=sde
+DRIVE_1=sdd1
+DRIVE_2=sde1
 
 sudo mdadm \
 	--create $DEST \
@@ -26,5 +26,10 @@ echo "Applied '$FS' file-system."
 
 NAS=/mnt/nas$INST
 sudo mkdir -p $NAS
-echo "$DEST $NAS $FS defaults 0 2" > $CWD/fstab
+echo "$DEST $NAS $FS defaults 0 2" >> $CWD/fstab
 echo "Updated '$CWD/fstab'."
+
+# Mount the drive so we can create the 'share' directory.
+sudo mount $DEST $NAS
+sudo mkdir $NAS/share
+sudo chown nobody.nogroup $NAS/share
