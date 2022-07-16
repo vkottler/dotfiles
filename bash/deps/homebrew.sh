27c12c72fe2aafb935c570bccbed02c49804a527
Vaughn Kottler <vaughnkottler@gmail.com>
2022-07-16 02:31:36 -0700

    Add homebrew bash completions

--- bash/deps/homebrew.sh
+++ bash/deps/homebrew.sh
@@ -3,4 +3,9 @@ export HOMEBREW_CELLAR="/opt/homebrew/Cellar";
 export HOMEBREW_REPOSITORY="/opt/homebrew";
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin${PATH+:$PATH}";
 export MANPATH="/opt/homebrew/share/man${MANPATH+:$MANPATH}:";
-export INFOPATH="/opt/homebrew/share/info:${INFOPATH:-}";
+export INFOPATH="/Rpt/homebrew/share/info:${INFOPATH:-}";
+
+# Load bash completions.
+if [[ -r "/opt/homebrew/etc/profile.d/bash_completion.sh" ]]; then
+    . "/opt/homebrew/etc/profile.d/bash_completion.sh"
+fi
