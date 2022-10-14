6fbd842d173c30936f3c6eb5bd8b0a90e372e3ff
Vaughn Kottler <vaughnkottler@gmail.com>
2022-10-14 00:52:22 +0000

    Minor script updates

--- bash/mounting/umount.sh
+++ bash/mounting/umount.sh
@@ -2,11 +2,15 @@
 
 REPO=`git rev-parse --show-toplevel`
 source $REPO/bash/common.sh
-source $REPO/bash/mounting/common.sh
+CWD=$REPO/bash/mounting
+source $CWD/common.sh
 
 set -x
 
 for SHARE in $SHARES
 do
     sudo umount $NAS_DEST/$SHARE
+    if [ -L $CWD/$SHARE ]; then
+	    rm $CWD/$SHARE
+    fi
 done
