fde7b47b1f8110803778c9c439e0de0a2b70419e
Vaughn Kottler <vaughnkottler@gmail.com>
2023-05-01 22:59:18 -0500

    Random fixes

--- bash/setup_ninja.sh
+++ bash/setup_ninja.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-REPO=`git rev-parse --show-toplevel`
-source $REPO/bash/common.sh
+REPO=$(git rev-parse --show-toplevel)
+source "$REPO/bash/common.sh"
 
 PROJ=ninja
 
 exit_if_command $PROJ
 call_setup python
 
-clone_third_party_github ninja-build $PROJ
+clone_third_party_github $PROJ-build $PROJ
 
-pushd $THIRD_PARTY/$PROJ >/dev/null
+pushd "$THIRD_PARTY/$PROJ" >/dev/null || exit
 
 # Build and install ninja.
 ./configure.py --bootstrap
 
 # Link documentation to the expected location.
-if [ ! -L $THIRD_PARTY_PREFIX/doc/ninja ]; then
-	mkdir -p $THIRD_PARTY_PREFIX/doc
-	ln -s $THIRD_PARTY/ninja/doc $THIRD_PARTY_PREFIX/doc/ninja
+if [ ! -L "$THIRD_PARTY_PREFIX/doc/$PROJ" ]; then
+	mkdir -p "$THIRD_PARTY_PREFIX/doc"
+	ln -s "$THIRD_PARTY/$PROJ/doc" "$THIRD_PARTY_PREFIX/doc/$PROJ"
 fi
 
 # Link binary so its on the path.
-if [ ! -L $THIRD_PARTY_PREFIX/bin/ninja ]; then
-	ln -s $THIRD_PARTY/ninja/ninja $THIRD_PARTY_PREFIX/bin/ninja
+if [ ! -L "$THIRD_PARTY_PREFIX/bin/$PROJ" ]; then
+	ln -s "$THIRD_PARTY/$PROJ/$PROJ" "$THIRD_PARTY_PREFIX/bin/$PROJ"
 fi
 
-popd >/dev/null
+popd >/dev/null || exit
